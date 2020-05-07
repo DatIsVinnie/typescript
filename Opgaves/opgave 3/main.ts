fbd3983d33955df356f4d00c9e2c75bd071ae201
DatIsVinnie <57494611+DatIsVinnie@users.noreply.github.com>
2020-05-07 11:34:16 +0200

    a

---
+++ Opgaves/opgave 3/main.ts
@@ -0,0 +1,17 @@
+var scope = 'superscope';
+var aantal = 0;
+
+function testScope() {
+
+    let scope = 'lokalescope';
+
+    aantal++;
+
+    console.log('\tDit is test nummer: ' + aantal);
+    console.log('\tFunction scope is: ' + scope);
+
+}
+
+console.log('\tScript scope is: ' + scope);
+testScope();
+testScope();
